0ccb34670f354e80b718ba65966fdef3c913e74b
Adányi Barnabás <adanyi@gmail.com>
2021-04-29 11:21:28 +0200

    Tested, completed.

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,15 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+    public int addDigits(String input) {
+
+        if (input == null || input.isEmpty()) {
+            return -1;
+        }
+
+        String digits = input.replaceAll("[^0-9]", "");
+
+        return digits.chars().map(it -> it - '0').sum();
+    }
+
 }
